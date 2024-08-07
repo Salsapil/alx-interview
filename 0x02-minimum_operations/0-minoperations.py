56ecf03d120240d8ce4d3d95693b527b5cf6154e
Salsabil <salsabeelahmed323@gmail.com>
2024-08-08 00:04:26 +0300

    0-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0. Minimum Operations"""
+
+def minOperations(n):
+    """Prime Factorization Concept"""
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            # While n is divisible by factor, add the factor to operations and divide n by factor
+            operations += factor
+            n /= factor
+        # Move to the next factor
+        factor += 1

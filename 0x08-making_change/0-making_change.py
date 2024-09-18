08667326edc4231a19db789756ebbfff2e460c76
Salsabil <salsabeelahmed323@gmail.com>
2024-09-19 01:36:51 +0300

    edit for eficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
 """0. Change comes from within"""
+from collections import deque
 
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize an array to store the fewest coins needed
-    # for each value from 0 to total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # No coins needed to make a total of 0
+    # Set to keep track of visited totals
+    # to avoid re-processing
+    visited = set()
+    visited.add(0)
 
-    # Iterate over each coin
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    # Initialize the queue with a
+    # tuple of (current total, number of coins used)
+    queue = deque([(0, 0)])
 
-    # If dp[total] is still inf, it means the total
-    # cannot be reached with the given coins
-    return dp[total] if dp[total] != float('inf') else -1
+    # Perform BFS
+    while queue:
+        current_total, coin_count = queue.popleft()
+
+        # Try all coins
+        for coin in coins:
+            new_total = current_total + coin
+
+            # If we reach the total, return the number of coins used
+            if new_total == total:
+                return coin_count + 1
+
+            # If the new total is less than total
+            # and has not been visited, add to queue
+            if new_total < total and new_total not in visited:
+                visited.add(new_total)
+                queue.append((new_total, coin_count + 1))
+
+    # If no solution was found, return -1
+    return -1
